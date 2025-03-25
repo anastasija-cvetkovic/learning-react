f9466cb9855f09f8fd0f2f1270890cd5b35cf7c8
Anastasija Cvetkovic <anastasija.cvetkovic1@outlook.com>
2025-03-25 22:09:20 +0100

    23:13

---
+++ youtube/nextjs by mosh/next-app/app/users/page.tsx
@@ -0,0 +1,8 @@
+//routing system in next js is based on convention, not configuration
+import React from "react";
+
+const UsersPage = () => {
+  return <div>UsersPage</div>;
+};
+
+export default UsersPage;
