4c361281da2eee2b2ccce84b0afe600e4717fdf8
Anastasija Cvetkovic <anastasija.cvetkovic1@outlook.com>
2025-03-30 22:25:33 +0200

    9:55:06

---
+++ youtube/full course by bob ziroll/section-4/meme-generator/src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main";
+import Header from "./components/Header";
+
+function App() {
+	return (
+		<>
+			<Main />
+			<Header />
+		</>
+	);
+}
+
+export default App;
